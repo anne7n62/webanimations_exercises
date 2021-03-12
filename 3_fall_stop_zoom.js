8bda32cc660391ff0a71efb75abdd4c2e0b121e4
Annemette <anne7n62@stud.kea.dk>
2021-03-12 11:17:15 +0100

    cancelling zoom and restarting fall

--- 3_fall_stop_zoom.js
+++ 3_fall_stop_zoom.js
@@ -1,44 +1,43 @@
 "use strict";
 
-// Create keyframes and properties for falling and zoom
+const ball = document.querySelector("#ball");
 
-const zoom = {
-  duration: 500,
-  iterations: 1,
-  direction: "alternate",
-  easing: "ease",
-  fill: "forwards",
-};
+// Create keyframes and properties for falling and zoom
 
 const falling = {
   duration: 1000,
   iterations: Infinity,
-  direction: "alternate",
-  easing: "ease",
+};
+
+const falling_kf = [{ transform: "translate(0,-20vw)" }, { transform: "translate(0,10vw)" }];
+
+const zoom = {
+  duration: 500,
+  fill: "forwards",
 };
 
 const zoom_kf = [{ transform: "scale(0)" }, { transform: "scale(1)" }];
 
-const falling_kf = [{ transform: "translateY(-20vw)" }, { transform: "translateY(10vw)" }];
-
 //Animating the ball using the  keyframes and properties
-const ball = document.querySelector("#ball");
 
 // register click
-document.querySelector("#ball").addEventListener("click", ballClicked);
+ball.addEventListener("mousedown", ballClicked);
 
 // start falling animation
-const animation = ball.animate(falling_kf, falling);
+const fall_anim = ball.animate(falling_kf, falling);
 
 function ballClicked() {
   console.log("Ball clicked!");
   // pause falling animation
-  animation.pause();
+  fall_anim.pause();
 
   // start zoom-animation
-  ballZoom();
-}
+  const zoom_anim = ball.animate(zoom_kf, zoom);
 
-function ballZoom() {
-  const animation = ball.animate(zoom_kf, zoom);
+  //when zoom finishes, cancel and start falling again
+  zoom_anim.onfinish = function () {
+    zoom_anim.cancel();
+    fall_anim.currentTime = 0;
+    fall_anim.play();
+  };
 }
